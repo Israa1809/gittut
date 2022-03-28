a0a9aec8280864aec9ad94420d7d715994ccb4f0
Israa1809 <din-israa@hotmail.com>
2022-03-28 11:32:26 +0200

    Added DieselTruck

--- src/com/Israa/DieselTruck.java
+++ src/com/Israa/DieselTruck.java
@@ -5,5 +5,6 @@ public class DieselTruck extends DieselCar
     public DieselTruck(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre, boolean particleFilter)
     {
         super(registrationNumber, make, model, numberOfDoors, kmPrLitre, particleFilter);
+
     }
 }
