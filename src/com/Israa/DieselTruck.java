d6534641d71cb13ff453a55299d5378190ac4e56
Israa1809 <din-israa@hotmail.com>
2022-03-28 11:28:43 +0200

    init implementation

---
+++ src/com/Israa/DieselTruck.java
@@ -0,0 +1,9 @@
+package com.Israa;
+
+public class DieselTruck extends DieselCar
+{
+    public DieselTruck(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre, boolean particleFilter)
+    {
+        super(registrationNumber, make, model, numberOfDoors, kmPrLitre, particleFilter);
+    }
+}
